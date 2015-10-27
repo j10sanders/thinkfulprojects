ee2c5eb6441adac4c870d32f4dd00650e40e0b85
j10sanders <jps458@nyu.edu>
2015-10-27 00:33:48 +0000

    first commit

---
+++ snippets.py
@@ -0,0 +1,77 @@
+__author__ = 'Jonathan'
+import logging
+import argparse
+import sys
+import psycopg2
+
+# Set the log output file, and the log level
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect("dbname='snippets'")
+logging.debug("Database connection established.")
+
+def put(name, snippet):
+    """Store a snippet with an associated name."""
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    with connection, connection.cursor() as cursor:
+        try:
+            command = "insert into snippets values (%s, %s)"
+            cursor.execute(command, (name, snippet))
+        except psycopg2.IntegrityError as e:
+            connection.rollback()
+            command = "update snippets set message=%s where keyword=%s"
+            cursor.execute(command, (snippet, name))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
+    return name, snippet
+
+def get(name):
+    """Retrieve the snippet with a given name."""
+    logging.info("Retrieving snippet {!r}".format(name))
+    cursor = connection.cursor()
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select message from snippets where keyword=%s", (name,))
+        row = cursor.fetchone()
+    logging.debug("Snippet retrieved successfully.")
+    if not row:
+        logging.debug("That keyword doesn't exist")
+        return "That keyword doesn't exist"
+    else:
+        return row[0]
+    
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+
+     # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Get a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+
+
+    arguments = parser.parse_args(sys.argv[1:])
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+
+
+
+if __name__ == "__main__":
+    main()
