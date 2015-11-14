e73fd902699ac3880e55f0d43f05cb803b6574a3
j10sanders <jps458@nyu.edu>
2015-11-14 22:53:32 +0000

    it works!

--- flask_hello_world/hello_world.py
+++ flask_hello_world/hello_world.py
@@ -14,21 +14,7 @@ def hello_person(name):
     
 @app.route("/jedi/<first_name>/<last_name>")
 def jedi_name(first_name, last_name):
-    return render_template('template.html')
-   
-'''   
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Your Jedi Name is {}
-        </p>
-     """
-    jedi_full_name = last_name[:3] + first_name[:2]
-    return html.format(first_name.title(), jedi_full_name.title())
-'''
-
+    return render_template('template_jedi.html', lastname = last_name, firstname = first_name)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
